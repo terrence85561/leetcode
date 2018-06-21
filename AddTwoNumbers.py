3aee2da788dff0b0e22c7560b7fda747cad5a8c7
Tianyi Liang <tianyi4@ualberta.ca>
2018-06-22 00:24:23 +0800

    Add2

---
+++ AddTwoNumbers.py
@@ -0,0 +1,43 @@
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        carry = 0
+        begin = ListNode(0)
+        current = begin
+
+
+        while l1 != None or l2 != None:
+            if l1 != None:
+                first = l1.val
+            else:
+                first = 0
+            if l2 != None:
+                second = l2.val
+            else:
+                second = 0
+
+            sums = first + second + carry
+            if sums >= 10:
+                carry = 1
+            else:
+                carry = 0
+
+            result = sums%10
+
+            current.next = ListNode(result)
+            current = current.next
+
+            if l1 != None:
+                l1 = l1.next
+            if l2 != None:
+                l2 = l2.next
+
+        if carry == 1:
+            temp = ListNode(1)
+            current.next = temp
+
+        return begin.next
