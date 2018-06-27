1eabbeec3745772d1aa87de2b3bf9d0868f678df
Tianyi Liang <tianyi4@ualberta.ca>
2018-06-27 22:20:33 +0800

    LS

--- LongestString.py
+++ LongestString.py
@@ -4,47 +4,39 @@ class Solution:
         :type s: str
         :rtype: int
         """
-        aList = []
-        pt_1 = 0
-        pt_2 = 0
-        flag = 1
-        answer = 1
 
-        while flag:
-            if len(s) == 0:
-                flag =0
-                answer =0
-                break
-            if s[pt_1] == s[pt_2]:
-                if s[pt_1] not in aList:
-                    aList.append(s[pt_1])
-                pt_2 = pt_2 + 1
-                if pt_2 == len(s):
-                    flag = 0
-                    break
-            if s[pt_1] != s[pt_2]:
-                if s[pt_1] not in aList:
-                    aList.append(s[pt_1 ])
-                pt_1 = pt_2 - 1
-                if s[pt_2] in aList:
-                    leng = len(aList)
-                    if leng > answer:
-                        answer = leng
-                    aList.clear()
-                    pt_1 = pt_2
-                    pt_2 += 1
-                else:
-                    aList.append(s[pt_2])
-                    pt_2 = pt_2 + 1
-                    if pt_2 == len(s):
-                        leng = len(aList)
-                        if leng > answer:
-                            answer = leng
-            if pt_2 == len(s):
-                flag = 0
-                break
+        dict = {} #for recording considering chars to determine repetition
+        answer = 0 #for output
+        pt1 = 0 #first pointer
+        leng = len(s)#length of input
+
+        #if input is empty
+        if leng == 0:
+            answer = 0
+        else:
+            #travers the whole input string use pt2
+            #if any chars between pt1 and pt2 repeats,
+            #pt1 move to the next char of the first repeated chars
+            #length of the longest substring is pt2 - pt1
+            for pt2 in range (0,leng):
+                if s[pt2] in dict:
+
+                    answer = max(answer,pt2 - pt1)
+                    firstIndex = dict.get(s[pt2])
+                    for j in range (pt1,firstIndex+1):
+                        dict.pop(s[j])
+                    pt1 = firstIndex + 1
+
+
+                if s[pt2] not in dict:
+                    dict[s[pt2]] = pt2
+                if pt2 == leng - 1 and dict:
+                    answer = max(len(dict),answer)
         return answer
+
+
+
 S = Solution()
-test = "aabcddddertyuiiiiiii"
+test = "aba"
 output = S.lengthOfLongestSubstring(test)
 print(output)
