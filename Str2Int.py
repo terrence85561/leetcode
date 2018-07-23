8403e601ae20fe48ef83a00f15a5200042507494
Tianyi Liang <tianyi4@ualberta.ca>
2018-07-23 12:08:11 -0700

    myAtoi

--- Str2Int.py
+++ Str2Int.py
@@ -5,17 +5,30 @@ class Solution:
         :type str: str
         :rtype: int
         """
-        import re
-        text = "t 123"
-    ptn = r"^ *\-*\d+"
-    result = re.findall(ptn,text)
-    print(result)
+        text = ''
+        ptn = r"^ *[\+\-]?\d+"
+        result = re.findall(ptn,str)
+        if result:
+            for i in result:
+                if i != ' ':
+                    text = text + i
+
+            output = int(text)
+        else:
+            return 0
+        if output < -2147483648:
+            output = -2147483648
+        elif output > 2147483647:
+            output = 2147483647
+        return output
+
+
 
 
 
 
 
 S = Solution()
-str = "   -42"
+str = " -+2"
 output = S.myAtoi(str)
 print(output)
