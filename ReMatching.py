378973dc03bfef055c39ea9b98db60371028041a
Tianyi Liang <tianyi4@ualberta.ca>
2018-07-26 00:05:46 -0700

    rema

--- ReMatching.py
+++ ReMatching.py
@@ -1,29 +1,23 @@
 class Solution:
     def isMatch(self, s, p):
-        """
-        :type s: original str
-        :type p: match str
-        :rtype: bool
-        """
-        start = 0
         sLen = len(s)
         pLen = len(p)
+        dp = [[False for i in range(0,pLen+1)]for j in range(0,sLen+1)]
+        dp[0][0] = True
 
-        dp = [[0 for i in range(0,sLen)]for j in range(0,pLen)]
+        for i in range(1,pLen+1):
+            if p[i-1] == '*':
+                if i >=2:
+                    dp[0][i] == dp[0][i-2]
+        for i in range(1,sLen+1):
+            for j in range(1,pLen+1):
+                if s[i-1] == p[j-1] or p[j-1] == '.':
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':
+                    if p[j-2] != s[i-1] or p[j-2] != '.':
+                        dp[i][j] = d[i][j-2]
 
-        for i in range(0,sLen):
-            for j in range(start,pLen):
-
-                if s[i] == s[j] and dp[j-1][i-1]:
-                    dp[j][i]=True
-                    start = j
-
-
-
-
-
-
-        return dp
+        return dp[sLen][pLen]
 
 
 
@@ -33,7 +27,7 @@ class Solution:
 
 
 S = Solution()
-s = 'mississippi'
-p = 'mis*is*p*.'
+s = 'b'
+p = 'b*'
 output = S.isMatch(s,p)
 print(output)
