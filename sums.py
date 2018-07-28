842f7ecbb6b1bd04108f72606de7d33659a08238
Tianyi Liang <tianyi4@ualberta.ca>
2018-07-27 23:29:18 -0700

    bla

--- sums.py
+++ sums.py
@@ -4,20 +4,11 @@ class Solution:
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
-
         """
 
-        for i in range(0,len(nums)) :
-            for j in range(0,len(nums)) :
-                if i == j:
-                    break
-                else:
-                    answer = nums[i] + nums[j]
-
-                    if target == answer:
-                        return(j,i)
-        """
-        for i in nums:
-            if target - i in nums and i is not target - i:
-                return(nums.index(i),nums.index(target - i))
-        """
+        dict = {}
+        for i in range (0,len(nums)):
+            if (target - nums[i]) in dict:
+                return(dict[target - nums[i]],i)
+            dict[nums[i]] = i
+        
