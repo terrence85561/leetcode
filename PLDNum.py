a33fce151dce07b8817d9a43b8eeb95bdc73f72e
Tianyi Liang <tianyi4@ualberta.ca>
2018-08-09 12:45:44 -0700

    pld

--- PLDNum.py
+++ PLDNum.py
@@ -4,25 +4,52 @@ class Solution:
         :type x: int
         :rtype: bool
         leetcode : 9 palindrome number
+        O(log(n))
         """
-        flag = False
-        if x < 0:
-            return flag
-        elif x < 10:
-            flag = True
-            return flag
-        else:
-            text = ''
-            reverse = ''
-            for i in str(x):
-                text += i
-            leng = len(text)
-            for j in range(leng - 1, -1,-1):
-                reverse += text[j]
 
-            if text == reverse:
-                flag = True
-            return flag
+
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+
+        else:
+            reverseInt = 0
+            # it's even
+            # 1221 % 10 = 1, 0*10 + 1 = 1
+            # 1221 // 10 = 122
+            # 122 % 10 = 2, 1 * 10 + 2 = 12
+            # 122 // 10 = 12
+            # reach half of int x, return
+            # it's odd
+            # 12221 % 10 = 1, 0 * 10 + 1 = 1
+            # 12221 // 10 = 1222
+            # 1222 % 10 = 2, 1*10 + 2 = 12
+            # 1222 // 10 = 122
+            # 122 % 10 = 2, 12 * 10 + 2 = 122
+            # 122 // 10 = 12
+            # reach half, return
+
+            while(x > reverseInt):
+                last = x % 10
+                reverseInt = last + reverseInt * 10
+                x = x // 10
+            return x == reverseInt or reverseInt // 10 == x
+
+
+
+
+
+            # text = ''
+            # reverse = ''
+            # for i in str(x):
+            #     text += i
+            # leng = len(text)
+            # for j in range(leng - 1, -1,-1):
+            #     reverse += text[j]
+            #
+            # if text == reverse:
+            #     flag = True
+            # return flag
 
 
 
@@ -36,6 +63,6 @@ class Solution:
 
 
 S = Solution()
-x = 122221
+x = 0
 output = S.isPalindrome(x)
 print(output)
--- medianOfTwoV2.py
+++ medianOfTwoV2.py
@@ -9,46 +9,51 @@ class Solution:
         using binarySearch
         cut2 = half - cut1
         condition:
-            1. nums1[cut1-1] < num2[cut2]
-            2. nums1[cut1] > num2[cut2 - 1]
+            1. nums1[cut1-1] =< num2[cut2]
+            2. nums1[cut1] >= num2[cut2 - 1]
         """
         m = len(nums1) # len of shorter array
         n = len(nums2)
-        if n < m:
-            # make sure nums1 is shorter than nums2
-            return(self.findMedianSortedArrays(nums2,nums1))
-        if m == 0:
-            if n % 2 == 0:
-                return (nums2[(n//2 - 1)]+nums2[n//2])/2
-            else:
-                return(nums2[n//2])
+        half = (m+n)//2
 
-        half = (m + n) // 2
+        #Min Max used for binary search
         Min = 0
         Max = m
+
+        if m > n: #ensure nums1 is the shorter array
+            return self.findMedianSortedArrays(nums2,nums1)
+
+        if m == 0:#if nums1 is null, find median in nums2
+            if n % 2 == 0:
+                return (nums2[(n//2)-1]+nums2[n//2])/2
+            else:
+                return nums2[n//2]
+
         while(Min <= Max):
-            cut1 = (Max + Min) // 2
+            cut1 = (Min + Max) // 2
             cut2 = half - cut1
-            if cut1 > 0 and nums1[cut1 - 1] > nums2[cut2] :
-                # first if cut1 > 0,then if nums[cut1 - 1]> nums2[cut2]
+            if cut1 > 0 and nums1[cut1 - 1] > nums2[cut2]:
                 Max = cut1 - 1
 
-            elif cut1 < m and nums1[cut1] < nums2[cut2 - 1] :
+            elif cut1 < m and nums1[cut1] < nums2[cut2 - 1]:
                 Min = cut1 + 1
+
             else:
                 if cut1 == 0:
-                    left = nums2[cut2-1]
+                    left = nums2[cut2 - 1]
                 elif cut2 == 0:
-                    left = nums1[cut1-1]
+                    left = nums1[cut1 - 1]
                 else:
-                    left = max(nums1[cut1-1],nums2[cut2-1])
+                    left = max(nums1[cut1 -1],nums2[cut2 - 1])
+
                 if cut1 == m:
                     right = nums2[cut2]
                 elif cut2 == n:
                     right = nums1[cut1]
                 else:
                     right = min(nums1[cut1],nums2[cut2])
-                if (m+n)%2 == 0:
-                    return(left + right)/2
+
+                if (m+n) % 2 == 0:
+                    return (left + right) /2
                 else:
                     return right
