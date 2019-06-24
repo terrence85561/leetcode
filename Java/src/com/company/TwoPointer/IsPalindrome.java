c4f35710fb20d64088346f91d372877525fcaf38
terrence85561 <tianyi4@ualberta.ca>
2019-06-24 00:10:06 -0700

    done 125, beat 83%

--- Java/src/com/company/TwoPointer/IsPalindrome.java
+++ Java/src/com/company/TwoPointer/IsPalindrome.java
@@ -5,12 +5,24 @@ package com.company.TwoPointer;
  */
 public class IsPalindrome {
     public boolean isPalindrome(String s) {
+        if(s.length()==0 || s==null) return true;
+        int sLength = s.length();
+        int l = 0, r = sLength - 1;
+        while(r>=l){
+            if(!Character.isLetterOrDigit(s.charAt(r))) r--;
+            else if(!Character.isLetterOrDigit(s.charAt(l))) l++;
+            else{
+                if(!(Character.toLowerCase(s.charAt(l))==Character.toLowerCase(s.charAt(r)))) return false;
+                l++;
+                r--;
+            }
+        }
         return true;
     }
 
     public static void main(String[] args) {
         IsPalindrome p = new IsPalindrome();
-        String s = "A man, a plan, a canal: Panama";
+        String s = "A an, a plan, a canal: Panama";
         boolean res = p.isPalindrome(s);
         System.out.println(res);
     }
