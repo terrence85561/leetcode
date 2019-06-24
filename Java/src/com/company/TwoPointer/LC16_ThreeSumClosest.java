35f58d28d6d449a1af593c47e01dacb4a8bb6650
terrence85561 <tianyi4@ualberta.ca>
2019-06-24 13:56:14 -0700

    done lc 16

--- Java/src/com/company/TwoPointer/LC16_ThreeSumClosest.java
+++ Java/src/com/company/TwoPointer/LC16_ThreeSumClosest.java
@@ -1,17 +1,43 @@
 package com.company.TwoPointer;
+
+import java.util.Arrays;
+
 /*
     lc 16
     two ptrs
  */
 public class LC16_ThreeSumClosest {
     public int threeSumClosest(int[] nums, int target) {
-        return 0;
+
+        Arrays.sort(nums);
+        int diff = Integer.MAX_VALUE;
+        int result = 0;
+        int numsLength = nums.length;
+
+        for(int i = 0; i < numsLength; i++){
+            int l = i+1, r = numsLength-1;
+            while(r>l){
+                if(Math.abs(nums[i]+nums[l]+nums[r]-target) < diff){
+                    diff = Math.abs(nums[i]+nums[l]+nums[r]-target);
+                    result = nums[i]+nums[l]+nums[r];
+                }
+                if(nums[i]+nums[l]+nums[r] < target) l++;
+                else if(nums[i]+nums[l]+nums[r] > target) r--;
+                else if(nums[i]+nums[l]+nums[r] == target) return target;
+
+            }
+        }
+        return result;
     }
+
+
+
+
 
     public static void main(String[] args) {
         LC16_ThreeSumClosest T = new LC16_ThreeSumClosest();
         int[] nums = {-1, 2, 1, -4};
-        int target = 1 ;
+        int target = -20 ;
         int res = T.threeSumClosest(nums, target);
         System.out.println(res);
     }
