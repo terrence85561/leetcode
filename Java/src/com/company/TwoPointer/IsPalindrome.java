276838f4b2e308056035b02cca74336354455eb8
terrence85561 <tianyi4@ualberta.ca>
2019-06-23 23:08:44 -0700

    start lc 125

---
+++ Java/src/com/company/TwoPointer/IsPalindrome.java
@@ -0,0 +1,18 @@
+package com.company.TwoPointer;
+/*
+  leetcode 125
+  two pointer
+ */
+public class IsPalindrome {
+    public boolean isPalindrome(String s) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        IsPalindrome p = new IsPalindrome();
+        String s = "A man, a plan, a canal: Panama";
+        boolean res = p.isPalindrome(s);
+        System.out.println(res);
+    }
+
+}
