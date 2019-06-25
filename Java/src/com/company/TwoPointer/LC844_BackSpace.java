eb81cd31593dc0b65c09ad9718f4d0407a094b2a
terrence85561 <tianyi4@ualberta.ca>
2019-06-25 11:23:28 -0700

    lc 844 using 2 ptr

--- Java/src/com/company/TwoPointer/LC844_BackSpace.java
+++ Java/src/com/company/TwoPointer/LC844_BackSpace.java
@@ -10,52 +10,62 @@ import java.util.Stack;
     Space : O(1)?
  */
 public class LC844_BackSpace {
-//    public boolean backspaceCompare(String S, String T) {
-//        int sPtr = S.length()-1;
-//        int tPtr = T.length()-1;
-//        int sBack = 0;
-//        int tBack = 0;
-//        for(int si = S.length()-1, ti = T.length()-1; si>=0 || ti >=0; si--, ti--){
-//            while(sPtr>0 && S.charAt(sPtr)=='#'){
-//                sPtr--;
-//            }
-//            while(tPtr>0 && T.charAt(tPtr)=='#'){
-//                tPtr--;
-//            }
-//
-//            if(S.charAt(si)=='#'){
-//                sBack++;
-//            }
-//            if(T.charAt(ti)=='#'){
-//                tBack++;
-//            }
-//
-//            while(sBack>0 && sPtr>=0){
-//                sPtr--;
-//                sBack--;
-//            }
-//            while(tBack>0 && tPtr>=0){
-//                tPtr--;
-//                tBack--;
-//            }
-//            if(sPtr<0 || tPtr<0){
-//                if(sPtr==tPtr) return true;
-//                else{
-//                    System.out.println(sPtr +" "+ tPtr);
-//                    return false;
-//                }
-//            }
-//
-//            if(S.charAt(sPtr)!=T.charAt(tPtr)){
-//                System.out.println(S.charAt(sPtr)+" "+T.charAt(tPtr));
-//                return false;
-//            }else{
-//                tPtr--;
-//                sPtr--;
-//            }
-//        }
-//        return true;
-//    }
+    
+    public boolean backspaceCompare(String S, String T){
+
+        int si = S.length()-1;
+        int ti = T.length()-1;
+
+        int sBack = 0;
+        int tBack = 0;
+
+        while(si>=0 || ti>=0){
+
+            while(si>=0 && (sBack>0 || S.charAt(si)=='#')){
+                if(S.charAt(si)=='#'){
+                    sBack++;
+                }else{
+                    sBack--;
+                }
+                si--;
+            }
+            char sChar, tChar;
+            if(si>=0){
+                sChar = S.charAt(si);
+            }else{
+                sChar = '@';
+            }
+
+
+            while(ti>=0 && (tBack>0 || T.charAt(ti)=='#')){
+                if(T.charAt(ti)=='#'){
+                    tBack++;
+                }else{
+                    tBack--;
+                }
+                ti--;
+            }
+
+            if(ti>=0){
+                tChar = T.charAt(ti);
+            }else{
+                tChar = '@';
+            }
+
+            if(sChar!=tChar) return false;
+            ti--;
+            si--;
+        }
+        return true;
+
+    }
+
+
+
+
+
+
+
     public boolean backspaceCompareWithStack(String S, String t){
 
         Stack<Character> s_stack = new Stack<>();
@@ -85,9 +95,9 @@ public class LC844_BackSpace {
 
     public static void main(String[] args) {
         LC844_BackSpace B = new LC844_BackSpace();
-        String s = "ab##";
+        String s = "ab#";
         String t = "c#d#";
-        boolean res = B.backspaceCompareWithStack(s, t);
+        boolean res = B.backspaceCompare(s, t);
         System.out.println(res);
     }
 }
