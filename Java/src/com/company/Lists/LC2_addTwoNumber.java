7c40c96ffa69ee0112804d7717c58666e096c99d
terrence85561 <tianyi4@ualberta.ca>
2019-06-25 23:19:05 -0700

    modified lc 2 coding style

--- Java/src/com/company/Lists/LC2_addTwoNumber.java
+++ Java/src/com/company/Lists/LC2_addTwoNumber.java
@@ -12,30 +12,14 @@ public class LC2_addTwoNumber {
         ListNode dummyhead = new ListNode(-1);
         ListNode curNode = dummyhead;
         int carry = 0;
-        ListNode l1_curNode = l1;
-        ListNode l2_curNode = l2;
-        while(l1_curNode!=null || l2_curNode!=null){
-            int l1_val, l2_val, res;
-            if(l1_curNode==null){
-                l1_val = 0;
-                l2_val = l2_curNode.val;
-                l2_curNode = l2_curNode.next;
-            }
-            else if(l2_curNode==null){
-                l1_val = l1_curNode.val;
-                l2_val = 0;
-                l1_curNode = l1_curNode.next;
-            }else{
-                l1_val = l1_curNode.val;
-                l2_val = l2_curNode.val;
-                l1_curNode = l1_curNode.next;
-                l2_curNode = l2_curNode.next;
-            }
-            res = (carry + l1_val + l2_val) % 10;
-            carry = (l1_val + l2_val + carry) / 10;
-
+        while(l1!=null || l2!=null){
+            int sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
+            int res = sum  % 10;
+            carry = sum / 10;
             curNode.next = new ListNode(res);
             curNode = curNode.next;
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
         }
 
         if(carry != 0){
