fe901f3c5d9098907cbe109d68085392157c3195
terrence85561 <tianyi4@ualberta.ca>
2019-06-27 14:09:24 -0700

    lc234 o(1) space with reverse half list

--- Java/src/com/company/Lists/LC234_palindromLinkedList.java
+++ Java/src/com/company/Lists/LC234_palindromLinkedList.java
@@ -7,6 +7,48 @@ import java.util.Stack;
     fast/slow pointers
  */
 public class LC234_palindromLinkedList {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null) return true;
+        // find the middle of LL
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode half_head = null;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        if(fast == null){
+            // the linkedList has length with an even numbe
+            half_head = reverse(slow);
+        }
+        else if(fast.next == null){
+            // the linkedList has a length with an odd number
+            // the slow ptr is the middle of LL
+            half_head = reverse(slow.next);
+        }
+
+
+        fast = head;
+        while(half_head != null){
+            if(fast.val != half_head.val) return false;
+            fast = fast.next;
+            half_head = half_head.next;
+        }
+        return true;
+    }
+
+
+    public ListNode reverse(ListNode head){
+        ListNode prev = null;
+        while(head != null){
+            ListNode curNode = head;
+            head = head.next;
+            curNode.next = prev;
+            prev = curNode;
+        }
+        return prev;
+    }
 
     public boolean isPalindromeWithStack(ListNode head) {
         /*
