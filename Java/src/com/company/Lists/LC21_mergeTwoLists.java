2df611b68cc8b3ee8153f7fd5f158d07790fe6ce
terrence85561 <tianyi4@ualberta.ca>
2019-06-26 22:49:36 -0700

    lc 21 recursive method done

--- Java/src/com/company/Lists/LC21_mergeTwoLists.java
+++ Java/src/com/company/Lists/LC21_mergeTwoLists.java
@@ -1,7 +1,10 @@
 package com.company.Lists;
 
 public class LC21_mergeTwoLists {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    public ListNode mergeTwoListsWithIteration(ListNode l1, ListNode l2) {
+        /*
+            iterative
+         */
         if(l1 == null) return l2;
         if(l2 == null) return l1;
         ListNode dummyhead = new ListNode(-1);
@@ -25,5 +28,24 @@ public class LC21_mergeTwoLists {
 
         return dummyhead.next;
 
+    }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        /*
+            recursive
+         */
+        // base case
+        if(l1 == null || l2 == null){
+            if(l1 == null) return l2;
+            else return l1;
+        }
+
+        if(l1.val <= l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else{
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
     }
 }
