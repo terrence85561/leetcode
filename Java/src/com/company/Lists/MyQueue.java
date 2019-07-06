41814370e5e28a0c2b2781cccfbef20fa1d1159c
terrence85561 <tianyi4@ualberta.ca>
2019-07-06 00:55:41 -0700

    done lc232 faster

--- Java/src/com/company/Lists/MyQueue.java
+++ Java/src/com/company/Lists/MyQueue.java
@@ -11,37 +11,49 @@ public class MyQueue {
 
     public MyQueue() {
         s1 = new Stack<>();
-        s2 = new Stack<>();
+//        s2 = new Stack<>();
 
     }
 
     /** Push element x to the back of queue. */
     public void push(int x) {
-        s1.push(x);
+//        s1.push(x);
+        s2 = new Stack<>();
+        while(!s1.isEmpty()){
+            s2.push(s1.pop());
+        }
+        s2.push(x);
+
+        while(!s2.isEmpty()){
+            s1.push(s2.pop());
+        }
     }
 
     /** Removes the element from in front of queue and returns that element. */
     public int pop() {
-        if(!s2.isEmpty()) return s2.pop();
-        while(!s1.isEmpty()){
-            s2.push(s1.pop());
-        }
-        return s2.pop();
+//        if(!s2.isEmpty()) return s2.pop();
+//        while(!s1.isEmpty()){
+//            s2.push(s1.pop());
+//        }
+//        return s2.pop();
+        return s1.pop();
     }
 
     /** Get the front element. */
     public int peek() {
-        if(!s2.isEmpty()) return s2.peek();
-        while(!s1.isEmpty()){
-            s2.push(s1.pop());
-        }
-        return s2.peek();
+//        if(!s2.isEmpty()) return s2.peek();
+//        while(!s1.isEmpty()){
+//            s2.push(s1.pop());
+//        }
+//        return s2.peek();
+        return s1.peek();
     }
 
     /** Returns whether the queue is empty. */
     public boolean empty() {
-        if(s1.isEmpty() && s2.isEmpty()) return true;
-        return false;
+//        if(s1.isEmpty() && s2.isEmpty()) return true;
+//        return false;
+        return s1.empty();
     }
 }
 
