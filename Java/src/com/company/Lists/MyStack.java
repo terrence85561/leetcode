f9e3cf4ae320a93f159b94077fa930553485aa13
terrence85561 <tianyi4@ualberta.ca>
2019-07-06 11:23:42 -0700

    done lc 225

--- Java/src/com/company/Lists/MyStack.java
+++ Java/src/com/company/Lists/MyStack.java
@@ -1,35 +1,48 @@
 package com.company.Lists;
 
-import java.util.Stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 /*
     LC225
  */
 public class MyStack {
 
+    Queue<Integer> q1, q2;
+
     /** Initialize your data structure here. */
     public MyStack() {
-        Stack<Integer> s1 = new Stack<>();
+        q1 = new LinkedList<>();
     }
 
     /** Push element x onto stack. */
     public void push(int x) {
+        // 2 -> 1 2 -> 1 -> 3 3->2->1
+        q1.offer(x);
+        int q1_size = q1.size();
+        int counter = 0;
+        while(counter < q1_size-1){
+            q1.offer(q1.poll());
+            counter++;
+        }
 
     }
 
     /** Removes the element on top of the stack and returns that element. */
     public int pop() {
-
+        return q1.poll();
     }
 
     /** Get the top element. */
     public int top() {
-
+        return q1.peek();
     }
 
     /** Returns whether the stack is empty. */
     public boolean empty() {
-
+        if(q1.isEmpty()) return true;
+        else return false;
     }
 }
 /**
