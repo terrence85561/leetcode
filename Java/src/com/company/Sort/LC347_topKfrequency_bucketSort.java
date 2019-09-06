674de182dbff3411ed83baf53f810d853341b9f9
terrence85561 <tianyi4@ualberta.ca>
2019-09-06 01:22:54 -0700

    leetcode:0347.java with bucketsort

---
+++ Java/src/com/company/Sort/LC347_topKfrequency_bucketSort.java
@@ -0,0 +1,42 @@
+package com.company.Sort;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class LC347_topKfrequency_bucketSort {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        /*
+            bucket sort
+
+            eg:
+                input [1,1,1,2,2,3]
+
+                hashmap {{1, 3}, {2, 2}, {3,1}}
+
+                bucket [ [],  [3],  [2], [1] ]
+        */
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+        }
+        List[] bucket = new List[nums.length+1];
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            if(bucket[entry.getValue()] == null){
+                bucket[entry.getValue()] = new LinkedList<>();
+            }
+            bucket[entry.getValue()].add(entry.getKey());
+        }
+
+        List<Integer> res = new LinkedList<>();
+        for(int i = bucket.length-1; i > 0 && res.size() < k; i--){
+            if(bucket[i] != null){
+                res.addAll(bucket[i]);
+            }
+        }
+        return res;
+
+    }
+}
